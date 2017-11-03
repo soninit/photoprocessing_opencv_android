4cde10b5661be6c190273329c0964bd601060133
sudo8890 <ttson.hcmus@gmail.com>
2017-11-03 18:08:07 +0700

    Update project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Photo Processing Using OpenCV in Android Framework
+
+An example using Android NDK & OpenCV to edit photo.
+
+## Feature implemented:
+
+- [X] Constrast & Brightness
+- [X] Crop
+- [X] Rotate
--- snappik/src/main/jni/Android.mk
+++ snappik/src/main/jni/Android.mk
@@ -2,12 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-OPENCV_ROOT:=/Users/admin/Desktop/opencv/OpenCV-android-sdk
+OPENCV_ROOT:=D:\dev\OpenCV-android-sdk
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCV_ROOT}/sdk/native/jni/OpenCV.mk
 
-NDK_MODULE_PATH=/Users/admin/Library/Android/sdk/ndk-bundle
+NDK_MODULE_PATH=D:\dev\androidndk
 LOCAL_ARM_NEON := true
 LOCAL_SRC_FILES := photosdk.cpp
 LOCAL_CPPFLAGS := -std=gnu++0x
